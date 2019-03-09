3e362961f74973bcf0a5461faa2a8316529b7aea
Saul Lopez <slopez@LAB-DIC-3.local>
2019-03-09 13:06:04 -0600

    corrijo error en el configTime

--- js/stopWatch.js
+++ js/stopWatch.js
@@ -17,7 +17,7 @@ class StopWatch {
     let btnStop = CreateDoom.btn('img/pause.png',{type: 'class', value: 'btn'});
     let btnReset = CreateDoom.btn('img/reset.png',{type: 'class', value: 'btn'});
 
-    timer.innerHTML = '--:--:--';
+    timer.innerHTML = this.configTime();
     btnStart.addEventListener('click', () => this.start(timer));
     btnStop.addEventListener('click', () => this.stop());
     btnReset.addEventListener('click', () => this.reset(timer));
@@ -30,26 +30,37 @@ class StopWatch {
     this.cont.appendChild(contStopWatch);
   }
 
-  time() {
-    this.hours = this.hours ? (this.hours < 10 ? `0${this.hours}` : this.hours) : '--';
-    this.minutes = this.minutes ? (this.minutes < 10 ? `0${this.minutes}` : this.minutes) : '--';
-    this.seconds = this.seconds < 10 ? `0${this.seconds}` : this.seconds;
+  configTime() {
+    this.seconds = parseInt(this.seconds);
+    this.minutes = parseInt(this.minutes);
+    this.hours = parseInt(this.hours);
+    if (this.seconds < 10) {
+      this.seconds = `0${this.seconds}`;
+    }
+    if (this.minutes < 10) {
+      this.minutes = `0${this.minutes}`;
+    }
+    if (this.hours < 10) {
+      this.hours = `0${this.hours}`;
+    }
     return `${this.hours}:${this.minutes}:${this.seconds}`;
   }
 
   start() {
+
     this._interval = setInterval(() => {
+      console.log(this.seconds);
       if(this.seconds < 59) {
         this.seconds ++;
       } else if (this.minutes < 59) {
         this.seconds = 0;
         this.minutes ++;
-      } else if (this.hours < 59) {
+      } else if (this.hours < 24) {
         this.minutes = 0;
         this.hours ++;
       }
 
-      this._time = this.time();
+      this._time = this.configTime();
       timer.innerHTML = this._time;
     }, 1000);
   } 
@@ -67,6 +78,6 @@ class StopWatch {
     this.minutes = 0;
     this.hours = 0;
     this.stop();
-    timer.innerHTML = this.time();
+    timer.innerHTML = this.configTime();
   }
 }
